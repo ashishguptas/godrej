5c0cf49560c43b282c61d5689f03a00dc7f7b122
Asg21 <ashishguptam1154@gmail.com>
2021-01-28 14:09:11 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,200 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+   <!-- Basic Page Needs ================================================== -->
+   
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta content="" name="keywords">
+
+    <link rel="icon" href="assets/images/logos/favicon.png" type="image/png" sizes="16x16">
+    <meta content="" name="description">
+    <title></title>
+
+
+
+   <!--  ====================== CSS ============================ -->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/animate.min.css">
+  <link rel="stylesheet" href="css/et-line-font.css">
+  <link rel="stylesheet" href="css/nivo-lightbox.css">
+  <link rel="stylesheet" href="css/nivo_themes/default/default.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500' rel='stylesheet' type='text/css'>
+      <script>
+function openNav() {
+  document.getElementById("myNav").style.width = "50%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+</script>
+
+</head>
+<body>
+
+<div id="myNav" class="overlay">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <div class="overlay-content">
+    <a href="">Home</a>
+    <a href="">Overview</a>
+    <a href="">Project</a>
+    <a href="">Amenities</a>
+    <a href="">Gallery</a>
+    <a href="">Location</a>
+  </div>
+</div>
+<div class="header">
+  <a href="index.php" class="logo">
+    <img src="images/godrejwood.png" class="img-fluid" alt="">
+  </a>
+    <div id="octagon" class="text-center">
+      <img src="images/logo.png" alt="" class="img-fluid">
+      <span onclick="openNav()">
+        <img src="images/spanbars.png" class="img-fluid" alt="">
+      </span>
+    </div>  
+</div>
+
+<div class="banner1 relative">
+  <img src="images/banner1.jpg" class="img-fluid" alt="" style="width: 100%;">
+  <div class="bannertext1">
+    <img src="images/bannertext1.png" class="img-fluid" alt="">
+  </div>
+  <div class="bannertext">
+    <img src="images/wherenaturelovesback.png" class="img-fluid" alt="">
+  </div>
+  <div class="vector1"><img src="images/vector1.png" class="img-fluid" alt=""></div>
+</div>
+
+<div class="container px-5 text-center overview">
+  <img src="images/overview.png" class="text-img" alt="">
+  <h2>A 40-hectare township in Mahalunge, Pune<br>
+nestled amidst a river and hill.</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
+Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+
+<div class="row">
+  <div class="col-md-2">
+    <div class="overview-img"><img src="images/overview1.png" class="text-img" alt=""></div>
+    <h5>Bio diversity park</h5>
+  </div>
+  <div class="col-md-2">
+    <div class="overview-img"><img src="images/overview2.png" class="text-img" alt=""></div>
+    <h5>2 themed clubhouse</h5>
+  </div>
+  <div class="col-md-2">
+    <div class="overview-img"><img src="images/overview3.png" class="text-img" alt=""></div>
+    <h5>50 species of trees</h5>
+  </div>
+  <div class="col-md-2">
+    <div class="overview-img"><img src="images/overview4.png" class="text-img" alt=""></div>
+    <h5>nature trail</h5>
+  </div>
+  <div class="col-md-2">
+    <div class="overview-img"><img src="images/overview5.png" class="text-img" alt=""></div>
+    <h5>waterside flora fauna park</h5>
+  </div>
+  <div class="col-md-2">
+    <div class="overview-img"><img src="images/overview6.png" class="text-img" alt=""></div>
+    <h5>birdwatching towers</h5>
+  </div>
+</div>
+</div>
+
+<div class="banner2 relative text-center">
+  <img src="images/banner2.jpg" class="img-fluid" alt="" style="width: 100%;">
+  <div class="bannertext2">
+    <img src="images/project.png" class="img-fluid" alt="">
+    <h2>Lorem ipsum dolor sit amet</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
+    Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+    <h5><img src="images/prevarrow.png" class="img-fluid" alt=""><img src="images/line.jpg" class="img-fluid" alt="">Lorem ipsum dolor sit amet<img src="images/line.jpg" class="img-fluid" alt=""><img src="images/nextarrow.png" class="img-fluid" alt=""></h5>
+  </div>
+  <div class="vector2"><img src="images/vector2.png" class="img-fluid" alt=""></div>
+</div>
+
+<div class="banner3 relative text-center">
+  <img src="images/banner3.png" class="img-fluid" alt="" style="width: 100%;">
+  <div class="bannertext3">
+    <img src="images/amenities.png" class="img-fluid" alt="">
+    <h2>Lorem ipsum dolor sit amet</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+  </div>
+  <div class="vector3"><img src="images/vector1.png" class="img-fluid" alt=""></div>
+</div>
+
+
+<div class="banner4 relative text-center">
+  <img src="images/banner4.jpg" class="img-fluid" alt="" style="width: 100%;">
+  <div class="bannertext4">
+    <img src="images/gallery.png" class="img-fluid" alt="">
+  </div>
+  <div class="vector4"><img src="images/vector2.png" class="img-fluid" alt=""></div>
+</div>
+
+<div class="banner5 relative text-center">
+  <img src="images/banner5.jpg" class="img-fluid" alt="" style="width: 100%;">
+  <div class="bannertext5">
+    <img src="images/location.png" class="img-fluid" alt="">
+  </div>
+  <div class="vector5"><img src="images/vector3.png" class="img-fluid" alt=""></div>
+</div>
+
+
+<div class="foot container-fluid">
+  <div class="foot-nav">
+    <div class="text-left">
+      <a href="index.php" class="logo">
+        <img src="images/godrejwood.png" class="img-fluid" alt="">
+      </a>
+    </div>
+    <div class="text-right">
+      <img src="images/logo.png" alt="" class="img-fluid">
+        <span onclick="openNav()">
+          <img src="images/spanbars.png" class="img-fluid" alt="">
+        </span>
+    </div>
+  </div>
+
+  <div class="foot-content container-fluid">
+    <h5><img src="images/line.jpg" class="img-fluid" width="30px" alt=""> SITE <img src="images/line.jpg" class="img-fluid" width="30px" alt=""></h5>
+    <h3>office</h3>
+    <p>Godrej Royale Woods, NH 44, Devanahalli, Bangalore - 562110.<br> Email ID- royalewoods@godrejproperties.com </p>
+    <h3>DISCLAIMER</h3>
+    <div class="row">
+      <div class="col-md-1"><img src="images/royalbuilder.jpg" class="img-fluid" alt=""></div>
+      <div class="col-md-11"><p>The project is developed by Godrej Projects North Star LLP, a part of Godrej Properties Group. Site office address: Godrej Royale Woods, NH 44, Devanahalli, Bangalore - 562110. RERA No.: PRAA/KA/RERA/1250/303/PR/200515/003411, available at website : http://rera .karnataka.gov. in </p></div>
+    </div>
+    <p>Acceptance or rejection of any booking shall be at AN discretion of the developer. The offer shall remain valid for limited time period. The offer shall be available for select projects of the developer. .An interest rate starting from 6.74% is applicable for a loan value up to Rs. 100 Lakh. Loan approval subject to eligibility of the customer. .Loan approval subject to eligibility criteria and terms and conditions as applicable and as may be provided by Godrej Housing Finance Ltd. Please visit www.godrejhf.com for terms and conditions. Terms & Conditions apply - for more information refer www.godrejhf.com The Developer is merely facilitating financing options for the Purchaser without any obligation or responsibility towards the funding or the loan eligibility/disbursement. All financing shall be under extant guidelines issued by Reserve Bank of India / National Housing Bank. The Ale is subject to terms of application form and Agreement for sale. All specifications of the unit shall be as per the final agreement between the Parties. Customers are advised to apprise themselves of the necessary and relevant information of the project / offer prior to making any purchase decisions. T&C Apply. The official website of Godrej Properties Ltd. is www.godrejproperties.com. Please do not rely on the information provided on any other website.</p>
+
+    <h3>PRIVACY POLICY DISCLAIMER </h3>
+    <p>We collect information from you when you register on our site or fill out a form. When filling out a form on our site, for any of the above-mentioned reasons, you may be asked to enter your: name, e-mail address and phone number. You may, however, visit our site anonymously. Any of the information we collect from you is to personalize your experience, to improve our website and to improve customer service. Any data collected will not be shared with any 3rd party. </p>
+
+  </div>
+
+  
+        
+</div>
+
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/isotope.js"></script>
+<script src="js/imagesloaded.min.js"></script>
+<script src="js/nivo-lightbox.min.js"></script>
+<script src="js/jquery.backstretch.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
